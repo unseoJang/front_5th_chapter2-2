4298e853090205c1deb7341e12361348858c22c9
unseoJang <qqbck123@naver.com>
2025-04-25 00:04:55 +0900

    배포

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import path from "path"
+import { resolve } from "path"
+
+const mode = process.env.NODE_ENV || "development"
+const env = loadEnv(mode, process.cwd(), "")
 
 export default mergeConfig(
 	defineConfig({
+		base: env.VITE_BASE_PATH,
 		plugins: [react()],
 		build: {
 			rollupOptions: {
 				input: {
-					main: path.resolve(__dirname, "index.refactoring.html"),
+					main: resolve(__dirname, "index.refactoring.html"),
 				},
 			},
 		},
 	}),
 	defineTestConfig({
-		plugins: [react()],
 		test: {
 			globals: true,
 			environment: "jsdom",
